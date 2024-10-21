d2583399d4d3e5e06e8c00c3fca15620d6bf3440
LNHBKDN <102210055@sv1.dut.udn.vn>
2024-10-22 02:59:16 +0700

    add onboarding screen

---
+++ craftshop2/lib/features/authencation/screens/onboarding/OnboardingNextButton.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/device/device_utility.dart';
+import '../../controllers/onboarding/onboarding_controller.dart';
+
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: CSDeviceUtils.getBottomNavigationBarHeight(),
+      right: CSSize.defaultSpace,
+      child: ElevatedButton(
+          onPressed: () => OnboardingController.instance.nextPage(),
+          style: ElevatedButton.styleFrom(shape: CircleBorder(), backgroundColor: Colors.black),
+          child: Icon(Iconsax.arrow_right_3)),
+    );
+  }
+}
--- craftshop2/lib/utils/constants/image_string.dart
+++ craftshop2/lib/utils/constants/image_string.dart
@@ -6,4 +6,9 @@ class CSImage{
   //-- Social logos
   static const String facebookLogo = 'assets/images/facebook.png';
   static const String googleLogo = 'assets/images/google.png';
+
+  //-- Onboarding Images
+  static const String onboarding1 = 'assets/images/on_boarding_images/searching.jpg';
+  static const String onboarding2 = 'assets/images/on_boarding_images/shopping.jpg';
+  static const String onboarding3 = 'assets/images/on_boarding_images/shipping.jpg';
 }
